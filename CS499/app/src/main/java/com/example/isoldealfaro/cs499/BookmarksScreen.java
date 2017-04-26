f4f7910efae3caf9fea2f9a5acceba11ecc5c2e3
hiisolde <ifmalfaro@gmail.com>
2017-04-25 19:44:09 -0700

    A3 complete

---
+++ CS499/app/src/main/java/com/example/isoldealfaro/cs499/BookmarksScreen.java
@@ -0,0 +1,13 @@
+package com.example.isoldealfaro.cs499;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookmarksScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookmarks_screen);
+    }
+}
--- CS499/app/src/main/java/com/example/isoldealfaro/cs499/MainMenu.java
+++ CS499/app/src/main/java/com/example/isoldealfaro/cs499/MainMenu.java
@@ -2,6 +2,9 @@ package com.example.isoldealfaro.cs499;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        ImageButton imageExample = (ImageButton) findViewById(R.id.mic);
+        imageExample.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent exIntent = new Intent(view.getContext(), LocationItem.class);
+                startActivityForResult(exIntent, 0);
+            }
+
+        });
+
+        ImageButton search = (ImageButton) findViewById(R.id.searchbutton);
+        search.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent searchIntent = new Intent(view.getContext(), SearchScreen.class);
+                startActivityForResult(searchIntent, 0);
+            }
+
+        });
+
+        ImageButton bookmark = (ImageButton) findViewById(R.id.bookmarkbutton);
+        bookmark.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent bookmarkIntent = new Intent(view.getContext(), BookmarksScreen.class);
+                startActivityForResult(bookmarkIntent, 0);
+            }
+
+        });
     }
+
 }
